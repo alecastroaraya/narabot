6be95be51c8a133164ee694f73dbbd836817151d
alejandrocastroarayaitcr <90380164+alecastroitcr@users.noreply.github.com>
2023-04-25 13:44:42 -0600

    Post tweet functionality added

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ def get_random_dialogue():
     with open('aranara_lines_ES.jsonl', 'r', encoding="utf8") as f: # Read the jsonl file with the dialogue lines and choose a random one
         lines = f.readlines()
         random_line = random.choice(lines)
-        dialogue = json.loads(random_line)
+        dialogue_line = json.loads(random_line)
 
-    return dialogue[0]
+    return dialogue_line[0]
 
 def format_dialogue_line(dialogue_line):
    return {"text": "{}".format(dialogue_line)}
@@ -31,4 +31,18 @@ def connect_to_oauth(consumer_key, consumer_secret, acccess_token, access_token_
    auth = OAuth1(consumer_key, consumer_secret, acccess_token, access_token_secret)
    return url, auth
 
-print(format_dialogue_line(get_random_dialogue()))
+def post_tweet():
+   dialogue_line = get_random_dialogue()
+   payload = format_dialogue_line(dialogue_line)
+
+   url, auth = connect_to_oauth(
+       consumer_key, consumer_secret, access_token, access_token_secret
+   )
+
+   request = requests.post(
+       auth=auth, url=url, json=payload, headers={"Content-Type": "application/json"}
+   )
+
+   print(request.text)
+
+post_tweet()
